991c205ea530c4a57d294e32ebcca68a6175a408
Guilherme Moraes <moraesguilherme91@gmail.com>
2024-06-21 15:28:38 -0300

    Ajustando css section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,40 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const SectionWrapper = styled.div`
-  margin: 16px 0;
-  background: #161B22;
-  border-radius: 8px;
-  padding: 16px;
-  width: 45%
-`;
-
-const SectionHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 16px;
-`;
-
-const SectionTitle = styled.h2`
-  margin: 0;
-  font-size: 24px;
-  color: white;
-`;
-
-const SectionLink = styled.a`
-  color: #F79E1B;
-  text-decoration: none;
-`;
+import './Section.css';
 
 const Section = ({ title, linkText, linkHref, children }) => (
-  <SectionWrapper>
-    <SectionHeader>
-      <SectionTitle>{title}</SectionTitle>
-      <SectionLink href={linkHref}>{linkText}</SectionLink>
-    </SectionHeader>
+  <div className="section-wrapper">
+    <div className="section-header">
+      <h2 className="section-title">{title}</h2>
+      <a href={linkHref} className="section-link">{linkText}</a>
+    </div>
     {children}
-  </SectionWrapper>
+  </div>
 );
 
 export default Section;
